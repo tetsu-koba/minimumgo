532d384cd50154980e64e164be2a6fef0b070f70
Tetsuyuki Kobayashi <tetsu.koba@gmail.com>
2018-04-14 21:42:11 +0900

    example: Copy /etc/localtime and so on at initramfs/build.sh

--- example/initramfs/build.sh
+++ example/initramfs/build.sh
@@ -1,9 +1,31 @@
 #!/bin/sh
 
+COPY_LOCALTIME=1
+#EXTRACT_ZONEINFO=1
+COPY_CA_CERTIFICATE=1
+
+ROOT=_root
 GOSRC=../httpfileserver/httpfileserver.go
 export GOARCH=arm
 export GOARM=6
 
-[ -d _root ] || mkdir _root 
-go build -ldflags '-s -w' -o _root/init $GOSRC
+[ -d $ROOT ] || mkdir $ROOT
+
+if [ "$COPY_LOCALTIME" ]; then
+    [ -d $ROOT/etc ] || mkdir $ROOT/etc
+    cp /etc/localtime $ROOT/etc/
+fi
+
+if [ "$EXTRACT_ZONEINFO" ]; then
+    [ -r zoneinfo.zip ] || wget https://github.com/golang/go/raw/master/lib/time/zoneinfo.zip
+    [ -d $ROOT/usr/share/zoneinfo ] || mkdir -p $ROOT/usr/share/zoneinfo
+    unzip zoneinfo.zip -d $ROOT/usr/share/zoneinfo/
+fi
+
+if [ "$COPY_CA_CERTIFICATE" ]; then
+    [ -d $ROOT/etc/ssl/certs ] || mkdir -p $ROOT/etc/ssl/certs
+    cp  /etc/ssl/certs/ca-certificates.crt $ROOT/etc/ssl/certs/
+fi
+
+go build -ldflags '-s -w' -o $ROOT/init $GOSRC
 fakeroot ./make_initramfs 
